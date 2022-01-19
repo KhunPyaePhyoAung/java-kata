628b0bb79cc50d6bc9175820f68a3a4c8414c69f
KhunPyaePhyoAung <khunpyaephyoaung74@gmail.com>
2022-01-19 13:55:55 +0630

    add Sum Of Numbers Range Kata

---
+++ src/test/java/me/khun/kata/fundamental/SumOfNumbersRangeTest.java
@@ -0,0 +1,18 @@
+package me.khun.kata.fundamental;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SumOfNumbersRangeTest {
+
+    @Test
+    public void Test1()
+    {
+        assertEquals(-1, SumOfNumbersRange.getSum(0, -1));
+        assertEquals(1, SumOfNumbersRange.getSum(0, 1));
+        assertEquals(0, SumOfNumbersRange.getSum(-1, 1));
+        assertEquals(12, SumOfNumbersRange.getSum(5, -2));
+        assertEquals(12, SumOfNumbersRange.getSum(-2, 5));
+    }
+}
