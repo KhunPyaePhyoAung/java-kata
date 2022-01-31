ebd89d950e0f11d8ed4bb2b89785923d5e70646c
KhunPyaePhyoAung <khunpyaephyoaung74@gmail.com>
2022-01-31 11:40:18 +0630

    add new test cases for Isograms Kata

--- src/test/java/me/khun/kata/fundamental/IsogramsTest.java
+++ src/test/java/me/khun/kata/fundamental/IsogramsTest.java
@@ -2,20 +2,65 @@ package me.khun.kata.fundamental;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
 
 public class IsogramsTest {
 
+    static final String AB = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    static SecureRandom rnd = new SecureRandom();
+
+    public String randomString( int len ){
+        StringBuilder sb = new StringBuilder( len );
+        for( int i = 0; i < len; i++ )
+            sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
+        return sb.toString();
+    }
+
     @Test
     public void Tests() {
-        assertEquals(true, Isograms.isIsogram("Dermatoglyphics"));
-        assertEquals(true, Isograms.isIsogram("isogram"));
-        assertEquals(false, Isograms.isIsogram("moose"));
-        assertEquals(false, Isograms.isIsogram("isIsogram"));
-        assertEquals(false, Isograms.isIsogram("aba"));
-        assertEquals(false, Isograms.isIsogram("moOse"));
-        assertEquals(true, Isograms.isIsogram("thumbscrewjapingly"));
-        assertEquals(true, Isograms.isIsogram(""));
+        assertTrue(Isograms.isIsogram("Dermatoglyphics"));
+        assertTrue(Isograms.isIsogram("isogram"));
+        assertFalse(Isograms.isIsogram("moose"));
+        assertFalse(Isograms.isIsogram("isIsogram"));
+        assertFalse(Isograms.isIsogram("aba"));
+        assertFalse(Isograms.isIsogram("moOse"));
+        assertTrue(Isograms.isIsogram("thumbscrewjapingly"));
+        assertTrue(Isograms.isIsogram(""));
+    }
+
+    @Test
+    public void FixedTests() {
+        assertTrue(Isograms.isIsogram("Dermatoglyphics"));
+        assertTrue(Isograms.isIsogram("isogram"));
+        assertFalse(Isograms.isIsogram("moose"));
+        assertFalse(Isograms.isIsogram("isIsogram"));
+        assertFalse(Isograms.isIsogram("aba"));
+        assertFalse(Isograms.isIsogram("moOse"));
+        assertTrue(Isograms.isIsogram("thumbscrewjapingly"));
+        assertTrue(Isograms.isIsogram(""));
+    }
+
+    private static boolean A(String str) {
+        str = str.toLowerCase();
+        int len = str.length();
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
+        for (int i = 0; i < len - 1; i++) {
+            if (arr[i] == arr[i + 1])
+                return false;
+        }
+        return true;
+    }
+
+    @Test
+    public void RandomTests() {
+        for(int i = 0; i < 101;) {
+            String Z = randomString(++i * 10);
+            assertEquals(A(Z), Isograms.isIsogram(Z));
+        }
     }
 
 }
