e7b985c8e68331031142af125755ec50e5dba29c
KhunPyaePhyoAung <khunpyaephyoaung74@gmail.com>
2022-01-31 11:34:23 +0630

    add new test cases for Sum Of Numbers Range Kata

--- src/test/java/me/khun/kata/fundamental/SumOfNumbersRangeTest.java
+++ src/test/java/me/khun/kata/fundamental/SumOfNumbersRangeTest.java
@@ -18,4 +18,37 @@ public class SumOfNumbersRangeTest {
         assertEquals(3, SumOfNumbersRange.getSum(3, 3));
 
     }
+
+    @Test
+    public void TestPositive() {
+        assertEquals(14, SumOfNumbersRange.getSum(5, -1));
+        assertEquals(127759, SumOfNumbersRange.getSum(505, 4));
+        assertEquals(44178, SumOfNumbersRange.getSum(321, 123));
+    }
+
+    @Test
+    public void TestNegative() {
+        assertEquals(-1275, SumOfNumbersRange.getSum(-50, 0));
+        assertEquals(-15, SumOfNumbersRange.getSum(-1, -5));
+        assertEquals(-5, SumOfNumbersRange.getSum(-5, -5));
+        assertEquals(-127755, SumOfNumbersRange.getSum(-505, 4));
+        assertEquals(-44055, SumOfNumbersRange.getSum(-321, 123));
+    }
+
+    @Test
+    public void TestZero() {
+        assertEquals(0, SumOfNumbersRange.getSum(0, 0));
+    }
+
+    @Test
+    public void TestSwitchMinMax() {
+        assertEquals(-15, SumOfNumbersRange.getSum(-5, -1));
+        assertEquals(15, SumOfNumbersRange.getSum(5, 1));
+    }
+
+    @Test
+    public void TestEqual() {
+        assertEquals(-17, SumOfNumbersRange.getSum(-17, -17));
+        assertEquals(17, SumOfNumbersRange.getSum(17, 17));
+    }
 }
