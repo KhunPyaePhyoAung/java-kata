b08595a30e7cdc1d6d0a3a56201928abcea25b4c
KhunPyaePhyoAung <khunpyaephyoaung74@gmail.com>
2022-01-31 11:28:35 +0630

    add new test case for Next Perfect Square Kata

--- src/test/java/me/khun/kata/fundamental/NextPerfectSquareTest.java
+++ src/test/java/me/khun/kata/fundamental/NextPerfectSquareTest.java
@@ -20,4 +20,23 @@ public class NextPerfectSquareTest {
         public void test3() {
             assertEquals(-1, NextPerfectSquare.findNextSquare(114));
         }
+
+    @Test
+    public void test4() {
+        assertEquals(15241630849L, NextPerfectSquare.findNextSquare(15241383936L));
+    }
+
+    @Test
+    public void test5() {
+        assertEquals(-1, NextPerfectSquare.findNextSquare(342786627));
+    }
+
+
+    @Test
+    public void randomTest1() {
+        long input = (long)(Math.random()*100000L)+1;
+        long square = input*input;
+
+        assertEquals(square+(input*2+1), NextPerfectSquare.findNextSquare(square));
+    }
 }
